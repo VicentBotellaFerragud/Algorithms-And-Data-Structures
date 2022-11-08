054a36e1cd1a90c02fe996e1a4ea4e29d807ba45
Vicent Botella <vb@interap.de>
2022-11-08 16:09:32 +0100

    conditions finished

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ function compareTwoValues(valueA, valueB) {
     let small, big;
 
     if (valueA > valueB) {
+
         small = valueB;
         big = valueA;
+
     } else {
+
         small = valueA;
         big = valueB;
+
     }
 
     compareTwoValuesResult.innerHTML = `${small}, ${big}`;
@@ -19,4 +23,39 @@ function compareTwoValues(valueA, valueB) {
 
     inputA.value = "";
     inputB.value = "";
-} 
+
+}
+
+//Loops
+
+let gameActive = false;
+
+function activateSpaceGame() {
+
+    gameActive = true;
+
+}
+
+function deactivateSpaceGame() {
+
+    gameActive = false;
+    hiddenMessageByDefault.innerHTML = "";
+
+}
+
+document.body.onkeyup = function (e) {
+
+    while (gameActive) {
+
+        if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+
+            hiddenMessageByDefault.innerHTML = `
+                This message is displayed because you pressed the space key after clicking the 'START' button.
+            `;
+            gameActive = false;
+
+        }
+
+    }
+
+}
